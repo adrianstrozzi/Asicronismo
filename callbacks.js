62d28be3b1e1f1063e033babeb45ec2765499ded
Adrian Strozzi <adrianstrozzi@gmail.com>
2020-03-27 14:55:49 -0600

    Created Promise

--- callbacks.js
+++ callbacks.js
@@ -2,33 +2,22 @@ const API_URL = 'https://swapi.co/api/'
 const PEOPLE_URL = 'people/:id'
 const opts = { crossDomain: true }
 
-function obtenerPersonaje(id, callback) {
-  const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
-
-
-  $.get(url, opts, callback).fail(() => {
-    console.log(`Sucedió un error. No se pudo obtener el personaje ${id}.`)
+function obtenerPersonaje(id) {
+  return new Promise((resolve, reject) => {
+    const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+    $.get(url, opts, function (data) {
+      resolve(data)
+    })
+      .fail(() => reject(id))
   })
 }
 
-obtenerPersonaje(1, function (character) {
-  console.log(`Hola yo soy ${character.name}, mido ${character.height} cm y nací el ${character.birth_year}.`)
+function onError(id) {
+  console.log(`Sucedió un error para obtener el personaje ${id}.`)
+}
 
-  obtenerPersonaje(2, function (character) {
+obtenerPersonaje(1)
+  .then(function (character) {
     console.log(`Hola yo soy ${character.name}, mido ${character.height} cm y nací el ${character.birth_year}.`)
-
-    obtenerPersonaje(3, function (character) {
-      console.log(`Hola yo soy ${character.name}, mido ${character.height} cm y nací el ${character.birth_year}.`)
-
-      obtenerPersonaje(4, function (character) {
-        console.log(`Hola yo soy ${character.name}, mido ${character.height} cm y nací el ${character.birth_year}.`)
-
-        obtenerPersonaje(5, function (character) {
-          console.log(`Hola yo soy ${character.name}, mido ${character.height} cm y nací el ${character.birth_year}.`)
-
-        })
-      })
-    })
   })
-})
-
+  .catch(onError)
