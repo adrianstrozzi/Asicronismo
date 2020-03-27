65ffe7387b95b452678a43f4cc059ea86c6e0d4b
Adrian Strozzi <adrianstrozzi@gmail.com>
2020-03-27 15:22:18 -0600

    Added async-await to return promises

--- callbacks.js
+++ callbacks.js
@@ -16,58 +16,17 @@ function onError(id) {
   console.log(`Sucedió un error para obtener el personaje ${id}.`)
 }
 
-// Creamos un array para que nos regresará 7 ids
-// En nuestra variable promesas utilizamos un método .map para obtener todos los ids
+// Implementación de un asyn-await
 
-var ids = [1, 2, 3, 4, 5, 6, 7]
-var promesas = ids.map(id => obtenerPersonaje(id))
+async function obtenerPersonajes() {
+  var ids = [1, 2, 3, 4, 5, 6, 7]
+  var promesas = ids.map(id => obtenerPersonaje(id))
+  try {
+    var characters = await Promise.all(promesas)
+    console.log(characters)
+  } catch (id) {
+    onError(id)
+  }
+}
 
-// Por último usamos métodos propios de las promesas para regresar todos los datos de los personajes.
-
-Promise
-  .all(promesas)
-  .then(characters => console.log(characters))
-  .catch(onError)
-
-// obtenerPersonaje(1)
-//   .then((character) => {
-//     console.log(`Hola yo soy ${character.name}, mido ${character.height} cm y nací el ${character.birth_year}.`)
-//     return obtenerPersonaje(2)
-//   })
-//   .then(character => {
-//     console.log(`Hola yo soy ${character.name}, mido ${character.height} cm y nací el ${character.birth_year}.`)
-//     return obtenerPersonaje(3)
-//   })
-//   .then(character => {
-//     console.log(`Hola yo soy ${character.name}, mido ${character.height} cm y nací el ${character.birth_year}.`)
-//     return obtenerPersonaje(4)
-//   })
-//   .then(character => {
-//     console.log(`Hola yo soy ${character.name}, mido ${character.height} cm y nací el ${character.birth_year}.`)
-//     return obtenerPersonaje(5)
-//   })
-//   .then(character => {
-//     console.log(`Hola yo soy ${character.name}, mido ${character.height} cm y nací el ${character.birth_year}.`)
-//     return obtenerPersonaje(6)
-//   })
-//   .then(character => {
-//     console.log(`Hola yo soy ${character.name}, mido ${character.height} cm y nací el ${character.birth_year}.`)
-//     return obtenerPersonaje(7)
-//   })
-//   .then(character => {
-//     console.log(`Hola yo soy ${character.name}, mido ${character.height} cm y nací el ${character.birth_year}.`)
-//     return obtenerPersonaje(8)
-//   })
-//   .then(character => {
-//     console.log(`Hola yo soy ${character.name}, mido ${character.height} cm y nací el ${character.birth_year}.`)
-//     return obtenerPersonaje(9)
-//   })
-//   .then(character => {
-//     console.log(`Hola yo soy ${character.name}, mido ${character.height} cm y nací el ${character.birth_year}.`)
-//     return obtenerPersonaje(10)
-//   })
-//   .then(character => {
-//     console.log(`Hola yo soy ${character.name}, mido ${character.height} cm y nací el ${character.birth_year}.`)
-//     return obtenerPersonaje(11)
-//   })
-//   .catch(onError)
+obtenerPersonajes()
