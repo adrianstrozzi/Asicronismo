ddeef9f483d5ba3c486cafc928a07681a1f9be41
Adrian Strozzi <adrianstrozzi@gmail.com>
2020-03-27 14:42:41 -0600

    Added fail method to avoid errors and ordered callbacks

--- callbacks.js
+++ callbacks.js
@@ -2,28 +2,29 @@ const API_URL = 'https://swapi.co/api/'
 const PEOPLE_URL = 'people/:id'
 const opts = { crossDomain: true }
 
-// Se agregó el parámetro callback a nuestra función
-
 function obtenerPersonaje(id, callback) {
   const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
 
-  $.get(url, opts, function (character) {
-    console.log(`Hola yo soy ${character.name}, mido ${character.height} cm y nací el ${character.birth_year}.`)
 
-    // Lo que va a hacer es que si tiene el parámetro callback se llamará a callback
-    if (callback) {
-      callback()
-    }
+  $.get(url, opts, callback).fail(() => {
+    console.log(`Sucedió un error. No se pudo obtener el personaje ${id}.`)
   })
 }
 
-// Si pedimos los personajes de esta manera aparecerán en orden
+obtenerPersonaje(1, function (character) {
+  console.log(`Hola yo soy ${character.name}, mido ${character.height} cm y nací el ${character.birth_year}.`)
 
-obtenerPersonaje(1, function () {
-  obtenerPersonaje(2, function () {
-    obtenerPersonaje(3, function () {
-      obtenerPersonaje(4, function () {
-        obtenerPersonaje(5, function () {
+  obtenerPersonaje(2, function (character) {
+    console.log(`Hola yo soy ${character.name}, mido ${character.height} cm y nací el ${character.birth_year}.`)
+
+    obtenerPersonaje(3, function (character) {
+      console.log(`Hola yo soy ${character.name}, mido ${character.height} cm y nací el ${character.birth_year}.`)
+
+      obtenerPersonaje(4, function (character) {
+        console.log(`Hola yo soy ${character.name}, mido ${character.height} cm y nací el ${character.birth_year}.`)
+
+        obtenerPersonaje(5, function (character) {
+          console.log(`Hola yo soy ${character.name}, mido ${character.height} cm y nací el ${character.birth_year}.`)
 
         })
       })
